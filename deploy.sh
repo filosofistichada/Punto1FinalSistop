e7fc256eba9cbece3ec6e716e80b1c8f1eb7b23e
filosofistichada <sofia.soto74@eia.edu.co>
2026-05-26 07:11:56 -0500

    Elementos

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 328065812392.dkr.ecr.us-east-2.amazonaws.com
 
-docker build -t lambda-final .
+docker build --provenance=false -t lambda-final .
 
 docker tag lambda-final:latest 328065812392.dkr.ecr.us-east-2.amazonaws.com/lambda-final:latest
 
